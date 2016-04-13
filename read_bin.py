34c8f4a8e5a83ba3f1851bf4a89c553f1bf434e9
Damanu <e.schwarzhans@gmx.at>
2016-04-13 12:55:10 +0200

    added python code that is soon to be reading the fortran binary file

---
+++ read_bin.py
@@ -0,0 +1,19 @@
+#/usr/bin/python!
+
+import numpy as np
+import sys
+from scipy.io import FortranFile
+BinaryData=sys.argv[1]
+def read():
+	filearray=FortranFile(BinaryData,'r')
+	print filearray
+	f=filearray.read_reals()
+	return f
+
+
+
+def main():
+	f=read()
+	print f
+if __name__=="__main__":
+	main()
