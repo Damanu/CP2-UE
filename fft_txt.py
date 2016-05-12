25ea572256aeb686b07c8b24557b9caf33dce1dd
Damanu <e.schwarzhans@gmx.at>
2016-05-12 10:41:43 +0200

    added and updated the fft_txt program

--- fft_txt.py
+++ fft_txt.py
@@ -1,27 +1,40 @@
 #usr/bin/python!
+#Calculates the fouriertransformed of a dataset in a text file 
+#and writes it into a txt file named fft_out.txt
 
 import sys
 import numpy as np
-
+if len(sys.argv)!=4:
+	print "No Argument given\nArguments: Filename xcolumn ycolumn"
+	sys.exit()
 FILE=sys.argv[1]
-COL=int(sys.argv[2])
+XCOL=int(sys.argv[2])
+YCOL=int(sys.argv[3])
 
 def main():
 	f=open(FILE,'r')
 	x=[]
+	y=[]
 	for line in f:
-		x.append(line.split()[COL])
+		y.append(line.split()[YCOL])
+		x.append(line.split()[XCOL])
 	x=map(float,x)	
-	print x
+	y=map(float,y)	
 	f.close()
-	y=np.fft.fft(x)
-	print(y)
+	ft=np.fft.fft(y)
+	delx=x[1]-x[0]#calculate delta x
+	omega_2=np.pi/delx#calculate nyquist frequency
+	w=np.linspace(-omega_2,omega_2,len(ft))
 	f=open("fft_out.txt","w")
 	i=0
-	for val in y:
-		f.write(str(i))
+	for val in ft:
+		f.write(str(w[i]))#print frequency
 		f.write("\t")
-		f.write(str(abs(val)))
+		f.write(str(abs(val)))#print absolute value
+		f.write("\t")
+		f.write(str(np.real(val)))#print real value
+		f.write("\t")
+		f.write(str(np.imag(val)))#print imaginary value
 		f.write("\n")
 		i+=1
 
