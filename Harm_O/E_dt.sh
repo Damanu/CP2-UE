f4923b06216afdbbf6942683152503657e2b455e
Damanu <e.schwarzhans@gmx.at>
2016-06-09 11:44:51 +0200

    added the plot over change of dt

--- Harm_O/E_dt.sh
+++ Harm_O/E_dt.sh
@@ -1,15 +1,65 @@
 #!/bin/bash
 
-dt=0.0001
+dt=0.001
 x=1
 v=0
-job=10000
+job=100
 pt=1
-c=0
+c=1
 
-while [ $c -lt 10 ] ; do
-	let c=c+1
-	echo $c
-	echo $dt $x $v $job $pt | ./init_holf 
-	./holf
-done
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.001.dat
+dt=0.002
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.002.dat
+dt=0.003
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.003.dat
+dt=0.004
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.004.dat
+dt=0.005
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.005.dat
+dt=0.006
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.006.dat
+dt=0.007
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.007.dat
+dt=0.008
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.008.dat
+dt=0.009
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.009.dat
+dt=0.01
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.01.dat
+dt=0.02
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.02.dat
+dt=0.03
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.03.dat
+dt=0.04
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.04.dat
+dt=0.05
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.05.dat
+dt=0.06
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.06.dat
+dt=0.07
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.07.dat
+dt=0.08
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.08.dat
+dt=0.09
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.09.dat
+dt=0.1
+printf "$dt\n$x\n$v\n$job\n$pt" |./init_holf  
+./holf > holf_dt=0.1.dat
---
+++ Harm_O/hosieu_dt.c
@@ -0,0 +1,65 @@
+/********************************************************************************
+*
+* File: holf.c
+*
+* Leapfrog simulation of a 1D harmonic oscillator
+* 
+* 06-06-2016
+*
+********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "getval.c"
+/*******************************************************************************/
+
+/*Global variables*/
+int ntjob, nt, ntprint;
+double x, v, dt, u, x_0, x_1, v_0, v_1;
+/*******************************************************************************/
+void getcf()	{
+	FILE *fpi;
+	
+	/* read startup file*/
+	fpi=fopen("ho_in.dat","r");
+	
+	fread(&dt,sizeof(double),1,fpi);
+	fread(&x_0,sizeof(double),1,fpi);
+	fread(&v_0,sizeof(double),1,fpi);
+	fread(&ntjob,sizeof(int),1,fpi);
+	fread(&ntprint,sizeof(int),1,fpi);
+	fclose(fpi);
+	
+	x_0=sin(dt);
+	v_0=cos(dt);
+}
+void move()	{
+	double F;
+	F=-x_0;
+	x=x_0+v_0*dt;
+	v=v_0+dt*F;
+	
+	v_0=v;
+	x_0=x;
+
+	return;	
+}
+void means()	{
+	u=(v_0*v_0+x_0*x_0)/2.0; //total energy
+	printf("%10d\t%12.5le\t%12.5le\t%12.5le\n",nt,u,x_0,v_0);
+	return;
+}		
+
+int main()	{
+	getcf();
+	for (dt=0.001;dt<=1;dt+=0.001) {
+		x_0=sin(dt);
+		v_0=cos(dt);
+		for(nt=0; nt<=1/dt; nt++) {
+			move();
+		}
+		printf("%12.5le\t",dt);
+		means();
+	}
+}
